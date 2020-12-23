21cb9083cadf876677d587d966d2fda36dd153af
William Touboul <Touboul.william@gmail.com>
2020-12-23 12:26:40 +0100

    Rendu Ex2

---
+++ exercice2.js
@@ -0,0 +1,10 @@
+var userName="Doe";
+var userFirstName="John";
+var town="New York";
+var breakLine ="\r\n"
+
+alert('Nom : '+ userName 
++ breakLine 
++ 'Prénom : ' + userFirstName
++ breakLine 
++ 'Ville : '+ town );
